82b93f85b488637f853f89dd0ec89903eea53f01
giedriuzz <kraujinis@gmail.com>
2025-04-04 13:23:57 +0300

    dirbaba toliaus su dictionary

--- src/dictionaries/raports.py
+++ src/dictionaries/raports.py
@@ -1,10 +1,30 @@
+"""
+Raportai
+
+Visada turi būti:
+- description
+- keys
+"""
+
 raport = {
     "radio_raports": {
         "salute": {
             "Pavadinimas": "SALUTE",
             "Aprašymas": "Pranešimai apie pastebėtą priešą",
         },
-        "salta": {"Pavadinimas": "SALTA", "Aprašymas": "Išsamus kontakto raportas"},
+        "salta": {
+                "description": "SALTA Išsamus kontakto raportas",
+                "keys": {
+                    "key_1" : "S - PAJĖGOS (Priešo pajėgos)",
+                    "key_2" : "A - PRIEŠO VEIKSMAI (Kas atsitiko)",
+                    "key_3" : "L - VIETA (Priešo ir draugiškų pajėgų dislokacijos vieta, jei yra žinoma)",
+                    "key_4" : "T - DATOS LAIKO GRUPĖ (DLG) (Tiesioginio ar vizualaus kontakto laikas)",
+                    "key_5" : "A - DRAUGIŠKŲ PAJĖGŲ VEIKSMAI (Kas padaryta, kas ketinama daryti)",
+                    "additional_1": "BŪTINA PAGALBA (Kokia reikalinga pagalba)",
+                    "additional_2": "Incidento kontrolės punkto (angl. ICP) vieta",
+                    "additional_3": "Saugus maršrutas iki incidento kontrolės punkto (angl. ICP)"}
+                },
+                  
         "walta": {"Pavadinimas": "WALTA", "Aprašymas": "Išsamus kontakto raportas"},
         "medevac": {"Pavadinimas": "MEDEVAC", "Aprašymas": "MEDEVAC raportas"},
         "locstat": {"Pavadinimas": "LOCSTAT", "Aprašymas": "Pranešimas apie padėtį"},
@@ -18,3 +38,7 @@ raport = {
         },
     }
 }
+
+""" KA ŠAUKIAME čia KAS ŠAUKIA, raportas SALTA "Klausau"
+    eilė Siera 
+"""
---
+++ src/read_raports.py
@@ -0,0 +1,20 @@
+from dictionaries.raports import raport
+
+
+
+def get_value_from_dictionary(
+    dictionary: dict, dictionary_key: str, value_name: str
+):
+    get_dictionary_key = dictionary[dictionary_key][value_name]
+    if get_dictionary_key["description"] is None:
+        print("")
+    else:
+        print(get_dictionary_key["description"])
+    for description in get_dictionary_key.get('keys'):
+        print(f"{get_dictionary_key['keys'][description]}")
+    
+    
+
+
+if __name__ == "__main__":
+    get_value_from_dictionary(dictionary=raport, dictionary_key="radio_raports", value_name="salta")
