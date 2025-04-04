49e0282602ede05b96731b0053c4eb0eb00df58a
giedriuzz <kraujinis@gmail.com>
2025-04-04 10:12:33 +0300

    working with dictionary

---
+++ src/dictionaries/raports.py
@@ -0,0 +1,20 @@
+raport = {
+    "radio_raports": {
+        "salute": {
+            "Pavadinimas": "SALUTE",
+            "Aprašymas": "Pranešimai apie pastebėtą priešą",
+        },
+        "salta": {"Pavadinimas": "SALTA", "Aprašymas": "Išsamus kontakto raportas"},
+        "walta": {"Pavadinimas": "WALTA", "Aprašymas": "Išsamus kontakto raportas"},
+        "medevac": {"Pavadinimas": "MEDEVAC", "Aprašymas": "MEDEVAC raportas"},
+        "locstat": {"Pavadinimas": "LOCSTAT", "Aprašymas": "Pranešimas apie padėtį"},
+        "casevac": {
+            "Pavadinimas": "CASEVAC",
+            "Aprašymas": "Pranešimas apie sužeistąjį, -uosius",
+        },
+        "sitpra": {
+            "Pavadinimas": "SITPRA",
+            "Aprašymas": "SITPRA (angl. SITREP) Situacijos raportas",
+        },
+    }
+}
