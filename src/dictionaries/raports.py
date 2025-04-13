c6af198a71711942a1589e42b23cf42d05259106
giedriuzz <kraujinis@gmail.com>
2025-04-13 10:09:29 +0300

    pridejau WALTA aprašymą, reikia pataisyti W aprašmą

--- src/dictionaries/raports.py
+++ src/dictionaries/raports.py
@@ -13,19 +13,31 @@ raport = {
             "Aprašymas": "Pranešimai apie pastebėtą priešą",
         },
         "salta": {
-                "description": "SALTA Išsamus kontakto raportas",
-                "keys": {
-                    "key_1" : "S - PAJĖGOS (Priešo pajėgos)",
-                    "key_2" : "A - PRIEŠO VEIKSMAI (Kas atsitiko)",
-                    "key_3" : "L - VIETA (Priešo ir draugiškų pajėgų dislokacijos vieta, jei yra žinoma)",
-                    "key_4" : "T - DATOS LAIKO GRUPĖ (DLG) (Tiesioginio ar vizualaus kontakto laikas)",
-                    "key_5" : "A - DRAUGIŠKŲ PAJĖGŲ VEIKSMAI (Kas padaryta, kas ketinama daryti)",
-                    "additional_1": "BŪTINA PAGALBA (Kokia reikalinga pagalba)",
-                    "additional_2": "Incidento kontrolės punkto (angl. ICP) vieta",
-                    "additional_3": "Saugus maršrutas iki incidento kontrolės punkto (angl. ICP)"}
-                },
-                  
-        "walta": {"Pavadinimas": "WALTA", "Aprašymas": "Išsamus kontakto raportas"},
+            "description": "SALTA Išsamus kontakto raportas",
+            "keys": {
+                "key_1": "S - PAJĖGOS (Priešo pajėgos)",
+                "key_2": "A - PRIEŠO VEIKSMAI (Kas atsitiko)",
+                "key_3": "L - VIETA (Priešo ir draugiškų pajėgų dislokacijos vieta, jei yra žinoma)",
+                "key_4": "T - DATOS LAIKO GRUPĖ (DLG) (Tiesioginio ar vizualaus kontakto laikas)",
+                "key_5": "A - DRAUGIŠKŲ PAJĖGŲ VEIKSMAI (Kas padaryta, kas ketinama daryti)",
+                "additional_1": "BŪTINA PAGALBA (Kokia reikalinga pagalba)",
+                "additional_2": "Incidento kontrolės punkto (angl. ICP) vieta",
+                "additional_3": "Saugus maršrutas iki incidento kontrolės punkto (angl. ICP)",
+            },
+        },
+        "walta": {
+            "description": "WALTA Išsamus kontakto raportas",
+            "keys": {
+                "key_1": "W - PAJĖGOS (Priešo pajėgos)",
+                "key_2": "A - PRIEŠO VEIKSMAI (Kas atsitiko)",
+                "key_3": "L - VIETA (Priešo ir draugiškų pajėgų dislokacijos vieta, jei yra žinoma)",
+                "key_4": "T - DATOS LAIKO GRUPĖ (DLG) (Tiesioginio ar vizualaus kontakto laikas)",
+                "key_5": "A - DRAUGIŠKŲ PAJĖGŲ VEIKSMAI (Kas padaryta, kas ketinama daryti)",
+                "additional_1": "BŪTINA PAGALBA (Kokia reikalinga pagalba)",
+                "additional_2": "Incidento kontrolės punkto (angl. ICP) vieta",
+                "additional_3": "Saugus maršrutas iki incidento kontrolės punkto (angl. ICP)",
+            },
+        },
         "medevac": {"Pavadinimas": "MEDEVAC", "Aprašymas": "MEDEVAC raportas"},
         "locstat": {"Pavadinimas": "LOCSTAT", "Aprašymas": "Pranešimas apie padėtį"},
         "casevac": {
--- src/read_raports.py
+++ src/read_raports.py
@@ -1,20 +1,17 @@
 from dictionaries.raports import raport
 
 
-
-def get_value_from_dictionary(
-    dictionary: dict, dictionary_key: str, value_name: str
-):
+def get_value_from_dictionary(dictionary: dict, dictionary_key: str, value_name: str):
     get_dictionary_key = dictionary[dictionary_key][value_name]
     if get_dictionary_key["description"] is None:
         print("")
     else:
         print(get_dictionary_key["description"])
-    for description in get_dictionary_key.get('keys'):
+    for description in get_dictionary_key.get("keys"):
         print(f"{get_dictionary_key['keys'][description]}")
-    
-    
 
 
 if __name__ == "__main__":
-    get_value_from_dictionary(dictionary=raport, dictionary_key="radio_raports", value_name="salta")
+    get_value_from_dictionary(
+        dictionary=raport, dictionary_key="radio_raports", value_name="walta"
+    )
