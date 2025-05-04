fdfc9355fdeb25547d6b3cc56516782d090fd1f5
giedriuzz <kraujinis@gmail.com>
2025-05-04 09:36:03 +0300

    pataisyti tyoe anotationai ir pataisyta tvarka kaip irasomi duomenys i zodyna

--- src/dictionaries/raports.py
+++ src/dictionaries/raports.py
@@ -2,90 +2,104 @@
 Raportai
 
 Visada turi būti:
-- description
-- keys
+- "description": "aprašymas"
+- "keys":
+    {
+        "key_1": ""
+    }
 """
 
+# type anotation: tuple[dict[str, str | dict[str, str]]]
+
 raport = {
-        "salute": {
-            "description": "SALUTE - pranešimai apie pastebėtą priešą",
-            "keys":{
-                
-            }
+    "salute": {
+        "description": "SALUTE - pranešimai apie pastebėtą priešą",
+        "keys": {
+            "key_1": "S - PAJĖGOS (priešo pajėgos)",
         },
-        "salta": {
-            "description": "SALTA - išsamus kontakto raportas",
-            "keys": {
-                "key_1": "S - PAJĖGOS (priešo pajėgos)",
-                "key_2": "A - PRIEŠO VEIKSMAI (kas atsitiko)",
-                "key_3": "L - VIETA (priešo ir draugiškų pajėgų dislokacijos vieta, jei yra žinoma)",
-                "key_4": "T - DATOS LAIKO GRUPĖ (DLG) (tiesioginio ar vizualaus kontakto laikas)",
-                "key_5": "A - DRAUGIŠKŲ PAJĖGŲ VEIKSMAI (kas padaryta, kas ketinama daryti)",
-                "additional_1": " BŪTINA PAGALBA (kokia reikalinga pagalba)",
-                "additional_2": " Incidento kontrolės punkto (angl. ICP) vieta",
-                "additional_3": " Saugus maršrutas iki incidento kontrolės punkto (angl. ICP)",
-            },
+    },
+    "salta": {
+        "description": "SALTA - išsamus kontakto raportas",
+        "keys": {
+            "key_1": "S - PAJĖGOS (priešo pajėgos)",
+            "key_2": "A - PRIEŠO VEIKSMAI (kas atsitiko)",
+            "key_3": "L - VIETA (priešo ir draugiškų pajėgų dislokacijos vieta, jei yra žinoma)",
+            "key_4": "T - DATOS LAIKO GRUPĖ (DLG) (tiesioginio ar vizualaus kontakto laikas)",
+            "key_5": "A - DRAUGIŠKŲ PAJĖGŲ VEIKSMAI (kas padaryta, kas ketinama daryti)",
+            "additional_1": " BŪTINA PAGALBA (kokia reikalinga pagalba)",
+            "additional_2": " Incidento kontrolės punkto (angl. ICP) vieta",
+            "additional_3": " Saugus maršrutas iki incidento kontrolės punkto (angl. ICP)",
         },
-        "walta": {
-            "description": "WALTA - išsamus kontakto raportas",
-            "keys": {
-                "key_1": "W - PAJĖGOS (kas dalyvauja)",
-                "key_2": "A - PRIEŠO VEIKSMAI (Kas atsitiko)",
-                "key_3": "L - VIETA (Priešo (koordinatės). Draugiškų pajėgų (nuorodos/koordinatės)",
-                "key_4": "T - DATOS LAIKO GRUPĖ (DLG) (tiesioginio ar vizualaus kontakto laikas)",
-                "key_5": "A - DRAUGIŠKŲ PAJĖGŲ VEIKSMAI (kas padaryta, kas ketinama daryti)",
-                "additional_1": " BŪTINA PAGALBA (kokia reikalinga pagalba)",
-                "additional_2": " Incidento kontrolės punkto (angl. ICP) vieta",
-                "additional_3": " Saugus maršrutas iki incidento kontrolės punkto (angl. ICP)",
-            },
+    },
+    "walta": {
+        "description": "WALTA - išsamus kontakto raportas",
+        "keys": {
+            "key_1": "W - PAJĖGOS (kas dalyvauja)",
+            "key_2": "A - PRIEŠO VEIKSMAI (Kas atsitiko)",
+            "key_3": "L - VIETA (Priešo (koordinatės). Draugiškų pajėgų (nuorodos/koordinatės)",
+            "key_4": "T - DATOS LAIKO GRUPĖ (DLG) (tiesioginio ar vizualaus kontakto laikas)",
+            "key_5": "A - DRAUGIŠKŲ PAJĖGŲ VEIKSMAI (kas padaryta, kas ketinama daryti)",
+            "additional_1": " BŪTINA PAGALBA (kokia reikalinga pagalba)",
+            "additional_2": " Incidento kontrolės punkto (angl. ICP) vieta",
+            "additional_3": " Saugus maršrutas iki incidento kontrolės punkto (angl. ICP)",
         },
-        "medevac": {"Pavadinimas": "MEDEVAC", "Aprašymas": "MEDEVAC raportas"},
-        "locstat": {"Pavadinimas": "LOCSTAT", "Aprašymas": "Pranešimas apie padėtį"},
-        "casevac": {
-            "Pavadinimas": "CASEVAC",
-            "Aprašymas": "Pranešimas apie sužeistąjį, -uosius",
+    },
+    "medevac": {
+        "description": "aprašymas",
+        "keys": {
+            "key_1": "A - DATOS LAIKO GRUPĖ (DLG)",
         },
-        "sitpra": {
-            "description": "SITPRA - situacijos raportas",
-            "keys": {
+    },
+    "locstat": {
+        "description": "aprašymas",
+        "keys": {
+            "keys_1": "A - DATOS LAIKO GRUPĖ (DLG)",
+        },
+    },
+    "casevac": {
+        "description": "CASEVAC - pranešimas apie sužeistąjį, -uosius",
+        "keys": {
+            "key_1": "užpildyti",
+        },
+    },
+    "sitpra": {
+        "description": "SITPRA - situacijos raportas",
+        "keys": {
             "key_1": "A - DATOS LAIKO GRUPĖ (DLG)",
             "key_2": "B - Vieta (savo pajėgų)",
             "key_3": "C - Kas atsitiko",
             "key_4": "D - Kas vyksta",
             "key_5": "E - Ketinimai",
-            
         },
-    }
-
-
-},
-alphabet = {
-            'A': "ALFA",
-            'B': "BRAVO",
-            'C': "CHARLIE",
-            'D': "DELTA",
-            'E': "ECHO",
-            'F': "FOXTROT",
-            'G': "GOLF",
-            'H': "HOTEL",
-            'I': "INDIA",
-            'J': "JULIET",
-            'K': "KILO",
-            'L': "LIMA",
-            'M': "MIKE",
-            'N': "NOVEMBER",
-            'O': "OSCAR",
-            'P': "PAPA",
-            'Q': "QUEBEC",
-            'R': "ROMEO",
-            'S': "SIERRA",
-            'T': "TANGO",
-            'U': "UNIFORM",
-            'V': "VICTOR",
-            'W': "WHISKEY",
-            'X': "XRAY",
-            'Y': "YANKEE",
-            'Z': "ZULU",
+    },
 }
 
 
+alphabet = {
+    "A": "ALFA",
+    "B": "BRAVO",
+    "C": "CHARLIE",
+    "D": "DELTA",
+    "E": "ECHO",
+    "F": "FOXTROT",
+    "G": "GOLF",
+    "H": "HOTEL",
+    "I": "INDIA",
+    "J": "JULIET",
+    "K": "KILO",
+    "L": "LIMA",
+    "M": "MIKE",
+    "N": "NOVEMBER",
+    "O": "OSCAR",
+    "P": "PAPA",
+    "Q": "QUEBEC",
+    "R": "ROMEO",
+    "S": "SIERRA",
+    "T": "TANGO",
+    "U": "UNIFORM",
+    "V": "VICTOR",
+    "W": "WHISKEY",
+    "X": "XRAY",
+    "Y": "YANKEE",
+    "Z": "ZULU",
+}
--- src/read_raports.py
+++ src/read_raports.py
@@ -1,46 +1,53 @@
-from dictionaries.raports import raport
-from dictionaries.raports import alphabet
+from typing import Any
 
+from dictionaries.raports import alphabet, raport
 
 radio_message_dictionary = []
 
 
 # gauname rtaporto aprašymą pagal įvestą raporto pavadinimą
 def get_raport_description_from_raport_dictionary(
-    dictionary: dict, dictionary_item: str
+    dictionary: dict[str, dict[str, Any]], dictionary_item: str
 ):
     try:
-        print(dictionary[0][dictionary_item]["description"])
+        description_of_raport = dictionary[dictionary_item]["description"]
+        radio_message_dictionary.append(str.upper(dictionary_item))
+        print(description_of_raport)
     except KeyError:
         print(f'Toks raportas "{dictionary_item}" nerastas')
-      
+
+
 def write_value_in_radio_message_dictionary(dictionary: dict, dictionary_item: str):
-    radio_message_dictionary.append(str.upper(dictionary_item))
-    get_dictionary_key = dictionary[0][dictionary_item]
+    # radio_message_dictionary.append(str.upper(dictionary_item))
+    get_dictionary_key = dictionary[dictionary_item]
     for key in get_dictionary_key.get("keys").items():
         input_string = input(f"{key[1]} : ")
         new_key_in_message = match_alphabet_letter(key[1][0])
-        radio_message_dictionary.append(
-                f'{new_key_in_message} - "{input_string}"'
-            )
-    
+        radio_message_dictionary.append(f'{new_key_in_message} - "{input_string}"')
+
+
 def format_radio_message(radio_message_dictionary: list):
     input_what_are_we_inviting = input("Ką kviečiame? ")
     input_who_invite = input("Kas kviečia? ")
     radio_message_dictionary.append(input_what_are_we_inviting)
     radio_message_dictionary.append(input_who_invite)
 
-def print_radio_raport(radio_message_dictionary: list):
-    print(f"{radio_message_dictionary[0]} aš {radio_message_dictionary[1]}, raportas {radio_message_dictionary[2]}, KLAUSAU")
-    print(f'"Laukite atsakymo iš {radio_message_dictionary[0]}"')
-    for i in radio_message_dictionary[3:]:
-        print(f'    eilė {i}')
 
-def match_alphabet_letter(letter:str) -> str:
+def print_radio_raport(radio_message_dictionary: list):
+    print(
+        f"{radio_message_dictionary[1]} aš {radio_message_dictionary[2]}, raportas {radio_message_dictionary[0]}, KLAUSAU"
+    )
+    print(f'"Laukite atsakymo iš {radio_message_dictionary[1]}"')
+    for i in radio_message_dictionary[3:]:
+        print(f"    eilė {i}")
+
+
+def match_alphabet_letter(letter: str) -> str:
     for x in alphabet.items():
         if x[0] == letter:
             return x[1]
-        
+    return ""
+
 
 if __name__ == "__main__":
     input_raport_name = input("Raporto pavadinimas: ")
@@ -55,6 +62,6 @@ if __name__ == "__main__":
 
     print_radio_raport(radio_message_dictionary)
     print(radio_message_dictionary)
-    print(raport[0]["salta"]["description"])
-    
+    print(raport["salta"]["description"])
+
     print(match_alphabet_letter("B"))
